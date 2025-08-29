387fddcb43b28ed9b3f1d0a91ee151d9fe721ca4
Shakil Ahmed <shakil.ahmed09@northsouth.edu>
2025-08-30 00:58:49 +0600

    new

---
+++ sk-portfolio/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.use(express.static(path.join(__dirname, 'dist/sk-portfolio/browser')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/sk-portfolio/browser/index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Frontend running on port ${port}`);
+});
