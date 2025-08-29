db6709bca28aa8b2f7283dddb44840874b8f61dd
Shakil Ahmed <shakil.ahmed09@northsouth.edu>
2025-08-29 19:40:53 +0600

    new commit

--- sk-portfolio/src/app/services/api.service.ts
+++ sk-portfolio/src/app/services/api.service.ts
@@ -108,7 +108,7 @@ export interface Review {
 })
 export class ApiService {
   // private apiUrl = 'https://portfolio-backend.onrender.com/api';
-   private apiUrl = environment.apiUrl;
+  private apiUrl = environment.apiUrl;
 
   constructor(private http: HttpClient) {}
 
--- sk-portfolio/src/environments/environment.ts
+++ sk-portfolio/src/environments/environment.ts
@@ -2,3 +2,4 @@ export const environment = {
   production: false,
   apiUrl: 'https://localhost:3000/api' // Your local backend URL
 };
+
