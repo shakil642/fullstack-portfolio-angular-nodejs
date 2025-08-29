8984947af1a3539276ee7b5edf9cdda60c2fe2e2
Shakil Ahmed <shakil.ahmed09@northsouth.edu>
2025-08-29 23:06:03 +0600

    new

--- sk-portfolio/src/environments/environment.prod.ts
+++ sk-portfolio/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://portfolio-backend.onrender.com/api' // Your live Render backend URL
+  apiUrl: 'https://portfolio-backend-hbc4.onrender.com/' // Your live Render backend URL
 };
