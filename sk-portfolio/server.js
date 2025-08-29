6691220be8ce02f4d9a4db628cece6489d7f67bf
Shakil Ahmed <shakil.ahmed09@northsouth.edu>
2025-08-30 02:28:43 +0600

    new

--- sk-portfolio/server.js
+++ sk-portfolio/server.js
@@ -1,20 +1,24 @@
-import cors from 'cors';
-
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
 
 const app = express();
 const port = process.env.PORT || 8080;
 
+app.use(
+  cors({
+    origin: [
+      'https://shakil64it.me',      // production
+      'http://localhost:4200',      // local dev
+    ],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true,
+  })
+);
+
 app.use(express.static(path.join(__dirname, 'dist/sk-portfolio/browser')));
 
-app.use(cors({
-  origin: ["https://shakil64it.me"], // only live site
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  credentials: true
-}));
-
-app.get('/*', (req, res) => {
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist/sk-portfolio/browser/index.csr.html'));
 });
 
